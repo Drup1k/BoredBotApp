be46a5892ecf6cb7879d7891bf609c65c6165df8
Andrej <andrej.zhdkv@gmail.com>
2023-12-07 14:41:32 +0100

    Add fun element & accessibility

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ fetch("https://apis.scrimba.com/bored/api/activity")
     .then(data => {
         console.log(data)
         document.getElementById("activity-name").textContent = data.activity;
+        //After clicking on button, text beneath will be showen
+        document.getElementById("title").textContent = "🦾 HappyBot 🦿";
+        document.body.classList.add("fun-color");
     })
 });
-
